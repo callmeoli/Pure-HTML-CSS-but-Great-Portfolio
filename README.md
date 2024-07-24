acf53d92988bf34df5c3d71125090df974d0ea5a
callmeolii <olanabackend@gmail.com>
2024-07-24 16:50:12 +0300

    initial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Ch1 begin: Establishing base styling 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jen4web/pen/mdgbOqL](https://codepen.io/jen4web/pen/mdgbOqL).
+
---
+++ dist/script.js
@@ -0,0 +1,17 @@
+/* 
+Welcome to the Getting Started with CSS workshop!
+
+In this first exercise, complete the following:
+
+- Sign into your CodePen account or create one if needed.
+
+- Make your Codepen show the rendered design on the right and the code blocks on the left by going to Change View (to the right of the Settings button up top).
+
+- Fork this CodePen and save to your account:
+https://blog.codepen.io/documentation/forks/
+
+- Follow along with Jen as we place the initial styling for this website.
+
+- Don't forget to save!
+
+*/
